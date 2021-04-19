43e3f11bb3a2c6c53bcd5f171d330d1dac46b7f5
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-04-19 18:12:09 +0400

    .

--- ops/cupy/kernels.cu.cc
+++ ops/cupy/kernels.cu.cc
@@ -9,11 +9,10 @@ __device__ complex<double> cadd(complex<double> a, complex<double> b) {
   return complex<double>(a.real() + b.real(), a.imag() + b.imag());
 }
 
-__device__ long multicontrol_index(long g, const int* qubits) {
+__device__ long multicontrol_index(const int* qubits, long g, int ncontrols) {
   long i = g;
-  const int ncontrols = (int)(sizeof(qubits) / sizeof(qubits[0]));
   for (int iq = 0; iq < ncontrols; iq++) {
-      const int n = qubits[iq];
+      const int n = qubits[2 * iq];
       long k = (long)(1 << n);
       i = ((long)((long)i >> n) << (n + 1)) + (i & (k - 1)) + k;
   }
@@ -96,47 +95,48 @@ __global__ void apply_swap_kernel(complex<double>* state, long tk1, long tk2,
 
 __global__ void multicontrol_apply_gate_kernel(complex<double>* state, long tk, int m,
                                                const complex<double>* gate,
-                                               const int* qubits) {
+                                               const int* qubits, int ncontrols) {
   const long g = blockIdx.x * blockDim.x + threadIdx.x;
-  const long i = multicontrol_index(g, qubits);
+  const long i = multicontrol_index(qubits, g, ncontrols);
   _apply_gate(state[i - tk], state[i], gate);
 }
 
 __global__ void multicontrol_apply_x_kernel(complex<double>* state, long tk, int m,
-                                            const int* qubits) {
+                                            const int* qubits, int ncontrols) {
   const long g = blockIdx.x * blockDim.x + threadIdx.x;
-  const long i = multicontrol_index(g, qubits);
+  const long i = multicontrol_index(qubits, g, ncontrols);
   _apply_x(state[i - tk], state[i]);
 }
 
 __global__ void multicontrol_apply_y_kernel(complex<double>* state, long tk, int m,
-                                            const int* qubits) {
+                                            const int* qubits, int ncontrols) {
   const long g = blockIdx.x * blockDim.x + threadIdx.x;
-  const long i = multicontrol_index(g, qubits);
+  const long i = multicontrol_index(qubits, g, ncontrols);
   _apply_y(state[i - tk], state[i]);
 }
 
 __global__ void multicontrol_apply_z_kernel(complex<double>* state, long tk, int m,
-                                            const int* qubits) {
+                                            const int* qubits, int ncontrols) {
   const long g = blockIdx.x * blockDim.x + threadIdx.x;
-  const long i = multicontrol_index(g, qubits);
+  const long i = multicontrol_index(qubits, g, ncontrols);
   _apply_z(state[i]);
 }
 
 __global__ void multicontrol_apply_z_pow_kernel(complex<double>* state, long tk, int m,
                                                 const complex<double>* gate,
-                                                const int* qubits) {
+                                                const int* qubits, int ncontrols) {
   const long g = blockIdx.x * blockDim.x + threadIdx.x;
-  const long i = multicontrol_index(g, qubits);
+  const long i = multicontrol_index(qubits, g, ncontrols);
   _apply_z_pow(state[i], gate[0]);
 }
 
 __global__ void multicontrol_apply_swap_kernel(complex<double>* state,
                                                long tk1, long tk2,
                                                int m1, int m2,
-                                               const int* qubits) {
+                                               const int* qubits,
+                                               int ncontrols) {
   const long g = blockIdx.x * blockDim.x + threadIdx.x;
-  const long i = multicontrol_index(g, qubits);
+  const long i = multicontrol_index(qubits, g, ncontrols);
   _apply_x(state[i - tk1], state[i - tk2]);
 }
 }
