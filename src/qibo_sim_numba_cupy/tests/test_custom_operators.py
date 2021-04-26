75d4a3ed4e10a94841f56e0be4a7a1871b28fa78
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-04-27 01:24:32 +0400

    Use qibo numpy backend for testing

--- src/qibo_sim_numba_cupy/tests/test_custom_operators.py
+++ src/qibo_sim_numba_cupy/tests/test_custom_operators.py
@@ -1,6 +1,9 @@
 import pytest
 import numpy as np
+import qibo
 from qibo_sim_numba_cupy import ops
+
+ATOL = {"complex64": 1e-6, "complex128": 1e-12}
 
 
 def qubits_tensor(nqubits, targets, controls=[]):
@@ -19,86 +22,54 @@ def random_state(nqubits, dtype="complex128"):
     return x / np.sqrt(np.sum(np.abs(x) ** 2))
 
 
-@pytest.mark.parametrize(("nqubits", "target", "einsum_str"),
-                         [(5, 4, "abcde,Ee->abcdE"),
-                          (4, 2, "abcd,Cc->abCd"),
-                          (4, 2, "abcd,Cc->abCd"),
-                          (3, 0, "abc,Aa->Abc"),
-                          (8, 5, "abcdefgh,Ff->abcdeFgh")])
+@pytest.mark.parametrize(("nqubits", "target", "controls"),
+                         [(5, 4, []), (4, 2, []), (4, 2, []), (3, 0, []),
+                          (8, 5, []), (3, 0, [1, 2]), (4, 3, [0, 1, 2]),
+                          (5, 3, [1]), (5, 2, [1, 4]), (6, 3, [0, 2, 5]),
+                          (6, 3, [0, 2, 4, 5])])
 @pytest.mark.parametrize("dtype", ["complex128", "complex64"])
-def test_apply_gate(nqubits, target, einsum_str, dtype):
+def test_apply_gate(nqubits, target, controls, dtype):
+    qibo.set_backend("numpy")
     state = random_state(nqubits, dtype=dtype)
-    gate = random_complex((2, 2), dtype=dtype)
+    matrix = random_complex((2, 2), dtype=dtype)
 
-    target_state = np.reshape(state, nqubits * (2,))
-    target_state = np.einsum(einsum_str, target_state, gate)
-    target_state = target_state.flatten()
-
-    qubits = qubits_tensor(nqubits, [target])
-    state = ops.apply_gate(state, gate, nqubits, target, qubits)
-    np.testing.assert_allclose(state, target_state)
-
-
-@pytest.mark.parametrize(("nqubits", "target", "controls", "einsum_str"),
-                         [(3, 0, [1, 2], "a,Aa->A"),
-                          (4, 3, [0, 1, 2], "a,Aa->A"),
-                          (5, 3, [1], "abcd,Cc->abCd"),
-                          (5, 2, [1, 4], "abc,Bb->aBc"),
-                          (6, 3, [0, 2, 5], "abc,Bb->aBc"),
-                          (6, 3, [0, 2, 4, 5], "ab,Bb->aB")])
-def test_apply_gate_controlled(nqubits, target, controls, einsum_str):
-    state = random_state(nqubits)
-    matrix = random_complex((2, 2))
-
-    target_state = np.reshape(state, nqubits * (2,))
-    slicer = nqubits * [slice(None)]
-    for c in controls:
-        slicer[c] = 1
-    slicer = tuple(slicer)
-    target_state[slicer] = np.einsum(einsum_str, target_state[slicer], gate)
-    target_state = target_state.flatten()
+    gate = qibo.gates.Unitary(matrix, target).controlled_by(*controls)
+    target_state = gate(np.copy(state))
 
     qubits = qubits_tensor(nqubits, [target], controls)
     state = ops.apply_gate(state, matrix, nqubits, target, qubits)
-    np.testing.assert_allclose(state, target_state)
+    np.testing.assert_allclose(state, target_state, atol=ATOL.get(dtype))
 
 
 @pytest.mark.parametrize(("nqubits", "target"),
                          [(3, 0), (4, 3), (5, 2), (3, 1)])
 @pytest.mark.parametrize("pauli", ["x", "y", "z"])
-def test_apply_pauli_gate(nqubits, target, pauli):
-    matrices = {"x": np.array([[0, 1], [1, 0]], dtype=np.complex128),
-                "y": np.array([[0, -1j], [1j, 0]], dtype=np.complex128),
-                "z": np.array([[1, 0], [0, -1]], dtype=np.complex128)}
+@pytest.mark.parametrize("dtype", ["complex128", "complex64"])
+def test_apply_pauli_gate(nqubits, target, pauli, dtype):
+    qibo.set_backend("numpy")
     state = random_state(nqubits)
-    gate = matrices[pauli]
+
+    gate = getattr(qibo.gates, pauli.capitalize())(target)
+    target_state = gate(np.copy(state))
 
     qubits = qubits_tensor(nqubits, [target])
-    target_state = np.copy(state)
-    target_state = ops.apply_gate(state, gate, nqubits, target, qubits)
-
     func = getattr(ops, "apply_{}".format(pauli))
     state = func(state, nqubits, target, qubits)
-    np.testing.assert_allclose(state, target_state)
+    np.testing.assert_allclose(state, target_state, atol=ATOL.get(dtype))
 
 
 @pytest.mark.parametrize(("nqubits", "target", "controls"),
                          [(3, 0, []), (3, 2, [1]),
                           (3, 2, [0, 1]), (6, 1, [0, 2, 4])])
 def test_apply_zpow_gate(nqubits, target, controls):
-    import itertools
-    phase = np.exp(1j * 0.1234)
-    qubits = list(controls)
-    qubits.append(target)
-    qubits.sort()
-    matrix = np.ones(2 ** nqubits, dtype=np.complex128)
-    for i, conf in enumerate(itertools.product([0, 1], repeat=nqubits)):
-        if np.prod(np.array(conf)[qubits]):
-            matrix[i] = phase
-
+    qibo.set_backend("numpy")
     state = random_state(nqubits)
-    target_state = np.diag(matrix).dot(state)
+    theta = 0.1234
 
+    gate = qibo.gates.ZPow(target, theta=theta).controlled_by(*controls)
+    target_state = gate(np.copy(state))
+
+    phase = np.exp(1j * theta)
     qubits = qubits_tensor(nqubits, [target], controls)
     state = ops.apply_z_pow(state, phase, nqubits, target, qubits)
     np.testing.assert_allclose(state, target_state)
