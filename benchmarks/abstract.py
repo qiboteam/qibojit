8832fcb9ea4defe69eb109920a9b772ade87142f
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-05-11 15:28:43 +0400

    Fix tensorflow benchmarks

--- benchmarks/abstract.py
+++ benchmarks/abstract.py
@@ -1,15 +1,18 @@
-import numpy as np
-
-
 class AbstractBackend:
 
     def __init__(self):
+        import numpy as np
+        self.np = np
         self.op = None
 
-    def cast(self, x, dtype="complex128"):
+    def cast(self, x, dtype=None):
+        if dtype is not None:
+            return self.np.array(x, dtype=dtype)
         return x
 
     def to_numpy(self, x, dtype="complex128"):
+        if dtype is not None:
+            return x.astype(dtype)
         return x
 
     def qubits_tensor(self, nqubits, targets, controls=[]):
@@ -18,7 +21,8 @@ class AbstractBackend:
         return sorted(qubits)
 
     def apply_gate_args(self, state, nqubits, controls=[]):
-        gate = np.array([[1, 1], [1, -1]], dtype=state.dtype)
+        gate = self.cast([[1, 1], [1, -1]], dtype=state.dtype)
+        gate = gate / self.np.sqrt(2)
         qubits = self.qubits_tensor(nqubits, [0], controls)
         return [state, gate, nqubits, 0, qubits]
 
@@ -35,12 +39,12 @@ class AbstractBackend:
         return [state, nqubits, 0, qubits]
 
     def apply_z_pow_args(self, state, nqubits, controls=[]):
-        gate = np.exp(1j * 0.1234)
-        qubits = self.qubits_tensor(nqubits, [0], controls)
+        gate = self.np.exp(1j * 0.1234)
+        qubits = self.np.self.qubits_tensor(nqubits, [0], controls)
         return [state, gate, nqubits, 0, qubits]
 
     def apply_two_qubit_gate_args(self, state, nqubits, controls=[]):
-        gate = np.random.random((4, 4)) + 1j * np.random.random((4, 4))
+        gate = self.np.random.random((4, 4)) + 1j * self.np.random.random((4, 4))
         gate = gate.astype(state.dtype)
         qubits = self.qubits_tensor(nqubits, [0, 1], controls)
         return [state, gate, nqubits, 0, 1, qubits]
@@ -50,10 +54,10 @@ class AbstractBackend:
         return [state, nqubits, 0, 1, qubits]
 
     def apply_fsim_args(self, state, nqubits, controls=[]):
-        gate = np.random.random(4) + 1j * np.random.random(4)
+        gate = self.np.random.random(4) + 1j * self.np.random.random(4)
         gate = gate.astype(state.dtype)
-        phase = np.array([np.exp(-1j * 0.1234)], dtype=state.dtype)
-        gate = np.concatenate([gate, phase])
+        phase = self.np.array([np.exp(-1j * 0.1234)], dtype=state.dtype)
+        gate = self.np.concatenate([gate, phase])
         qubits = self.qubits_tensor(nqubits, [0, 1], controls)
         return [state, gate, nqubits, 0, 1, qubits]
 
@@ -68,13 +72,13 @@ class AbstractBackend:
 
     def qft(self, state, nqubits):
         import numpy as np
-        matrix = np.array([[1, 1], [1, -1]], dtype=state.dtype)
-        matrix = self.cast(matrix / np.sqrt(2))
+        matrix = self.np.array([[1, 1], [1, -1]], dtype=state.dtype)
+        matrix = self.cast(matrix / self.np.sqrt(2))
         for i1 in range(nqubits):
             qubits = self.qubits_tensor(nqubits, [i1])
             state = self.apply_gate(state, matrix, nqubits, i1, qubits)
             for i2 in range(i1 + 1, nqubits):
-                theta = self.cast(np.pi / 2 ** (i2 - i1))
+                theta = self.cast(self.np.pi / 2 ** (i2 - i1))
                 qubits = self.qubits_tensor(nqubits, [i1], [i2])
                 state = self.apply_z_pow(state, theta, nqubits, i1, qubits)
 
