540304d6e9c654ff550a617ef3b090a72cd2c332
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-05-12 23:16:33 +0400

    Avoid function duplication

--- src/qibojit/custom_operators/__init__.py
+++ src/qibojit/custom_operators/__init__.py
@@ -7,7 +7,7 @@ class Backend:
 
     def __init__(self):
         self.constructed_backends = {}
-        self.set_backend("numba")
+        self.active_backend = self.construct_backend("numba")
 
     def construct_backend(self, name):
         if name not in self.constructed_backends:
@@ -18,55 +18,55 @@ class Backend:
         return self.constructed_backends.get(name)
 
     def set_backend(self, name):
-        self.backend = self.construct_backend(name)
+        self.active_backend = self.construct_backend(name)
 
-    def apply_gate(self, state, gate, nqubits, target, qubits=None):
-        return self.backend.one_qubit_base(state, nqubits, target, "apply_gate", qubits, gate)
-
-    def apply_x(self, state, nqubits, target, qubits=None):
-        return self.backend.one_qubit_base(state, nqubits, target, "apply_x", qubits)
-
-    def apply_y(self, state, nqubits, target, qubits=None):
-        return self.backend.one_qubit_base(state, nqubits, target, "apply_y", qubits)
-
-    def apply_z(self, state, nqubits, target, qubits=None):
-        return self.backend.one_qubit_base(state, nqubits, target, "apply_z", qubits)
-
-    def apply_z_pow(self, state, gate, nqubits, target, qubits=None):
-        return self.backend.one_qubit_base(state, nqubits, target, "apply_z_pow", qubits, gate)
-
-    def apply_two_qubit_gate(self, state, gate, nqubits, target1, target2, qubits=None):
-        return self.backend.two_qubit_base(state, nqubits, target1, target2,
-                                           "apply_two_qubit_gate",
-                                           qubits, gate)
-
-    def apply_swap(self, state, nqubits, target1, target2, qubits=None):
-        return self.backend.two_qubit_base(state, nqubits, target1, target2,
-                                           "apply_swap", qubits)
-
-    def apply_fsim(self, state, gate, nqubits, target1, target2, qubits=None):
-        return self.backend.two_qubit_base(state, nqubits, target1, target2,
-                                           "apply_fsim", qubits, gate)
-
-    def initial_state(self, nqubits, dtype, is_matrix=False):
-        return self.backend.ops.initial_state(nqubits, dtype, is_matrix)
-
-    def collapse_state(self, state, qubits, result, nqubits, normalize):
-        return self.backend.ops.collapse_state(state, qubits, result, nqubits, normalize)
-
-    def measure_frequencies(self, frequencies, probs, nshots, nqubits, seed=1234):
-        return self.backend.ops.measure_frequencies(frequencies, probs, nshots, nqubits, seed)
+    def __getattr__(self, x):
+        return getattr(self.active_backend, x)
 
 
 backend = Backend()
-apply_gate = backend.apply_gate
-apply_x = backend.apply_x
-apply_y = backend.apply_y
-apply_z = backend.apply_z
-apply_z_pow = backend.apply_z_pow
-apply_two_qubit_gate = backend.apply_two_qubit_gate
-apply_swap = backend.apply_swap
-apply_fsim = backend.apply_fsim
-initial_state = backend.initial_state
-collapse_state = backend.collapse_state
-measure_frequencies = backend.measure_frequencies
+
+def apply_gate(state, gate, nqubits, target, qubits=None):
+    return backend.one_qubit_base(state, nqubits, target, "apply_gate", qubits, gate)
+
+
+def apply_x(state, nqubits, target, qubits=None):
+    return backend.one_qubit_base(state, nqubits, target, "apply_x", qubits)
+
+
+def apply_y(state, nqubits, target, qubits=None):
+    return backend.one_qubit_base(state, nqubits, target, "apply_y", qubits)
+
+
+def apply_z(state, nqubits, target, qubits=None):
+    return backend.one_qubit_base(state, nqubits, target, "apply_z", qubits)
+
+
+def apply_z_pow(state, gate, nqubits, target, qubits=None):
+    return backend.one_qubit_base(state, nqubits, target, "apply_z_pow", qubits, gate)
+
+
+def apply_two_qubit_gate(state, gate, nqubits, target1, target2, qubits=None):
+    return backend.two_qubit_base(state, nqubits, target1, target2,
+                                  "apply_two_qubit_gate", qubits, gate)
+
+
+def apply_swap(state, nqubits, target1, target2, qubits=None):
+    return backend.two_qubit_base(state, nqubits, target1, target2, "apply_swap", qubits)
+
+
+def apply_fsim(state, gate, nqubits, target1, target2, qubits=None):
+    return backend.two_qubit_base(state, nqubits, target1, target2,
+                                  "apply_fsim", qubits, gate)
+
+
+def initial_state(nqubits, dtype, is_matrix=False):
+    return backend.ops.initial_state(nqubits, dtype, is_matrix)
+
+
+def collapse_state(state, qubits, result, nqubits, normalize):
+    return backend.ops.collapse_state(state, qubits, result, nqubits, normalize)
+
+
+def measure_frequencies(frequencies, probs, nshots, nqubits, seed=1234):
+    return backend.ops.measure_frequencies(frequencies, probs, nshots, nqubits, seed)
