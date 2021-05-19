f7e02e62eb78b65edf35e4d0b7564fd5912ed9dd
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-05-19 19:25:34 +0400

    Update dtype parametrization

--- src/qibojit/tests/conftest.py
+++ src/qibojit/tests/conftest.py
@@ -11,15 +11,7 @@ def backend(backend_name):
 
 
 def pytest_generate_tests(metafunc):
-    # TODO: Fix `complex64` for cupy
     if "backend_name" in metafunc.fixturenames:
-        if "dtype" in metafunc.fixturenames:
-            metafunc.parametrize("backend_name,dtype",
-                                 [("numba", "complex128"),
-                                  ("numba", "complex64"),
-                                  ("cupy", "complex128"),
-                                  ("cupy", "complex64")])
-        else:
-            metafunc.parametrize("backend_name", ["numba", "cupy"])
-    elif "dtype" in metafunc.fixturenames:
+        metafunc.parametrize("backend_name", ["numba", "cupy"])
+    if "dtype" in metafunc.fixturenames:
         metafunc.parametrize("dtype", ["complex128", "complex64"])
