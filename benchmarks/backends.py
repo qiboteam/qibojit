4bd7c34479ce865efda7daae77fa00d8f1ad5200
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-05-21 15:56:26 +0400

    Add cupy backend to benchmark script

--- benchmarks/backends.py
+++ benchmarks/backends.py
@@ -13,6 +13,19 @@ class NumbaBackend(AbstractBackend):
         return tuple(qubits)
 
 
+class CupyBackend(AbstractBackend):
+
+    def __init__(self):
+        super().__init__()
+        from qibojit import custom_operators as op
+        op.set_backend("cupy")
+        self.op = op
+
+    def qubits_tensor(self, nqubits, targets, controls=[]):
+        qubits = super().qubits_tensor(nqubits, targets, controls)
+        return self.cast(qubits, dtype="int32")
+
+
 class TensorflowBackend(AbstractBackend):
 
     def __init__(self):
@@ -34,7 +47,7 @@ class TensorflowBackend(AbstractBackend):
 
     def qubits_tensor(self, nqubits, targets, controls=[]):
         qubits = super().qubits_tensor(nqubits, targets, controls)
-        return self.cast(qubits, dtype=self.backend.int32)
+        return self.cast(qubits, dtype="int32")
 
     def apply_gate(self, state, gate, nqubits, target, qubits):
         return self.op.apply_gate(state, gate, qubits, nqubits, target, self.nthreads)
@@ -48,32 +61,14 @@ class TensorflowBackend(AbstractBackend):
     def apply_z(self, state, nqubits, target, qubits):
         return self.op.apply_z(state, qubits, nqubits, target, self.nthreads)
 
-    def apply_z_pow_args(self, state, nqubits, controls=[]):
-        gate = self.cast(self.np.exp(1j * 0.1234), dtype=state.dtype)
-        qubits = self.qubits_tensor(nqubits, [0], controls)
-        return [state, gate, nqubits, 0, qubits]
-
     def apply_z_pow(self, state, gate, nqubits, target, qubits):
         return self.op.apply_z_pow(state, gate, qubits, nqubits, target, self.nthreads)
-
-    def apply_two_qubit_gate_args(self, state, nqubits, controls=[]):
-        gate = self.np.random.random((4, 4)) + 1j * self.np.random.random((4, 4))
-        gate = self.cast(gate, dtype=state.dtype)
-        qubits = self.qubits_tensor(nqubits, [0, 1], controls)
-        return [state, gate, nqubits, 0, 1, qubits]
 
     def apply_two_qubit_gate(self, state, gate, nqubits, target1, target2, qubits):
         return self.op.apply_two_qubit_gate(state, gate, qubits, nqubits, target1, target2, self.nthreads)
 
     def apply_swap(self, state, nqubits, target1, target2, qubits):
         return self.op.apply_swap(state, qubits, nqubits, target1, target2, self.nthreads)
-
-    def apply_fsim_args(self, state, nqubits, controls=[]):
-        gate = list(self.np.random.random(4) + 1j * self.np.random.random(4))
-        gate.append(self.np.exp(-1j * 0.1234))
-        gate = self.cast(gate, dtype=state.dtype)
-        qubits = self.qubits_tensor(nqubits, [0, 1], controls)
-        return [state, gate, nqubits, 0, 1, qubits]
 
     def apply_fsim(self, state, gate, nqubits, target1, target2, qubits):
         return self.op.apply_fsim(state, gate, qubits, nqubits, target1, target2, self.nthreads)
@@ -94,17 +89,14 @@ class TensorflowBackend(AbstractBackend):
     def measure_frequencies(self, frequencies, probs, nshots, nqubits):
         return self.op.measure_frequencies(frequencies, probs, nshots, nqubits, 1234, self.nthreads)
 
-    def measure_frequencies_args(self, state, nqubits, controls=[]):
-        if controls:
-            raise NotImplementedError
-        frequencies = self.backend.zeros(state.shape, dtype="int64")
-        probs = self.backend.square(self.backend.abs(state))
-        return [frequencies, probs]
-
 
 class Backends(dict):
 
-    _implemented = {"numba": NumbaBackend, "tensorflow": TensorflowBackend}
+    _implemented = {
+        "numba": NumbaBackend,
+        "cupy": CupyBackend,
+        "tensorflow": TensorflowBackend,
+        }
 
     def get(self, name):
         if name not in self:
