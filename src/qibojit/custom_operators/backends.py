Fix collapse normalization

src/qibojit/custom_operators/backends.py
@@ -59,7 +59,7 @@ class NumbaBackend(AbstractBackend):
     def to_numpy(self, x):
         if isinstance(x, self.np.ndarray):
             return x
-        return self.np.array(x)        
+        return self.np.array(x)
 
     def free_all_blocks(self):
         pass
@@ -99,7 +99,9 @@ class NumbaBackend(AbstractBackend):
         return self.ops.initial_state_vector(nqubits, dtype)
 
     def collapse_state(self, state, qubits, result, nqubits, normalize=True):
-        return self.ops.collapse_state(state, qubits, result, nqubits, normalize)
+        if normalize:
+            return self.ops.collapse_state_normalized(state, qubits, result, nqubits)
+        return self.ops.collapse_state(state, qubits, result, nqubits)
 
     def measure_frequencies(self, frequencies, probs, nshots, nqubits, seed=1234, nthreads=None):
         if nthreads is None:

src/qibojit/custom_operators/ops.py
@@ -29,7 +29,21 @@ def collapse_index(g, h, qubits):
 
 
 @njit(parallel=True, cache=True)
-def collapse_state(state, qubits, result, nqubits, normalize=True):
+def collapse_state(state, qubits, result, nqubits):
+    qubits = tuple(qubits)
+    nstates = 1 << (nqubits - len(qubits))
+    nsubstates = 1 << len(qubits)
+
+    for g in prange(nstates):  # pylint: disable=not-an-iterable
+        for h in range(result):
+            state[collapse_index(g, h, qubits)] = 0
+        for h in range(result + 1, nsubstates):
+            state[collapse_index(g, h, qubits)] = 0
+    return state
+
+
+@njit(parallel=True, cache=True)
+def collapse_state_normalized(state, qubits, result, nqubits):
     qubits = tuple(qubits)
     nstates = 1 << (nqubits - len(qubits))
     nsubstates = 1 << len(qubits)
@@ -42,12 +56,10 @@ def collapse_state(state, qubits, result, nqubits, normalize=True):
         for h in range(result + 1, nsubstates):
             state[collapse_index(g, h, qubits)] = 0
 
-    if normalize:
-        norm = np.sqrt(norms)
-        for g in prange(nstates):  # pylint: disable=not-an-iterable
-            i = collapse_index(g, result, qubits)
-            state[i] = state[i] / norm
-
+    norm = np.sqrt(norms)
+    for g in prange(nstates):  # pylint: disable=not-an-iterable
+        i = collapse_index(g, result, qubits)
+        state[i] = state[i] / norm
     return state
 
 