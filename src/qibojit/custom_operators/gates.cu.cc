967f891a818a9b82ef16b32e2f0d503886e0ea05
Marco Lazzarin <48728634+mlazzarin@users.noreply.github.com>
2021-10-23 11:06:16 +0400

    Use thrust::complex in device functions of gates.cu.cc

--- src/qibojit/custom_operators/gates.cu.cc
+++ src/qibojit/custom_operators/gates.cu.cc
@@ -1,22 +1,22 @@
 #include <cupy/complex.cuh>
 
 
-__device__ complex<double> cmult(complex<double> a, complex<double> b) {
-  return complex<double>(a.real() * b.real() - a.imag() * b.imag(),
-                         a.real() * b.imag() + a.imag() * b.real());
+__device__ thrust::complex<double> cmult(thrust::complex<double> a, thrust::complex<double> b) {
+  return thrust::complex<double>(a.real() * b.real() - a.imag() * b.imag(),
+                                 a.real() * b.imag() + a.imag() * b.real());
 }
 
-__device__ complex<float> cmult(complex<float> a, complex<float> b) {
-  return complex<float>(a.real() * b.real() - a.imag() * b.imag(),
-                        a.real() * b.imag() + a.imag() * b.real());
+__device__ thrust::complex<float> cmult(thrust::complex<float> a, thrust::complex<float> b) {
+  return thrust::complex<float>(a.real() * b.real() - a.imag() * b.imag(),
+                                a.real() * b.imag() + a.imag() * b.real());
 }
 
-__device__ complex<double> cadd(complex<double> a, complex<double> b) {
-  return complex<double>(a.real() + b.real(), a.imag() + b.imag());
+__device__ thrust::complex<double> cadd(thrust::complex<double> a, thrust::complex<double> b) {
+  return thrust::complex<double>(a.real() + b.real(), a.imag() + b.imag());
 }
 
-__device__ complex<float> cadd(complex<float> a, complex<float> b) {
-  return complex<float>(a.real() + b.real(), a.imag() + b.imag());
+__device__ thrust::complex<float> cadd(thrust::complex<float> a, thrust::complex<float> b) {
+  return thrust::complex<float>(a.real() + b.real(), a.imag() + b.imag());
 }
 
 __device__ long multicontrol_index(const int* qubits, long g, int ncontrols) {
