5fc2264baba1cd13fe840079218b7a2c4624cb8c
Stavros <35475381+stavros11@users.noreply.github.com>
2022-06-26 17:06:28 +0300

    Implement multigpu

--- src/qibojit/backends.py
+++ src/qibojit/backends.py
@@ -221,7 +221,7 @@ class NumbaBackend(NumpyBackend):
     #def assert_allclose(self, value, target, rtol=1e-7, atol=0.0): Inherited from ``NumpyBackend``
 
 
-class CupyBackend(NumpyBackend):
+class CupyBackend(NumbaBackend):
 
     DEFAULT_BLOCK_SIZE = 1024
     MAX_NUM_TARGETS = 7
@@ -233,6 +233,7 @@ class CupyBackend(NumpyBackend):
         import cupy_backends  # pylint: disable=import-error
         self.name = "qibojit"
         self.platform = "cupy"
+        self.supports_multigpu = True
         self.numeric_types = (int, float, complex, cp.int32,
                               cp.int64, cp.float32, cp.float64,
                               cp.complex64, cp.complex128)
@@ -284,6 +285,9 @@ class CupyBackend(NumpyBackend):
         # load numba op for measuring frequencies
         from qibojit.custom_operators.ops import measure_frequencies
         self.measure_frequencies_op = measure_frequencies
+
+        # number of available GPUs (for multigpu)
+        self.ngpus = cp.cuda.runtime.getDeviceCount()
 
     def set_precision(self, precision):
         super().set_precision(precision)
@@ -459,6 +463,68 @@ class CupyBackend(NumpyBackend):
 
     #def reset_error_density_matrix(self, gate, state, nqubits): Inherited from ``NumpyBackend``
 
+    def execute_distributed_circuit(self, circuit, initial_state=None, nshots=None):
+        import joblib
+        from qibo.states import CircuitResult
+        
+        if not circuit.queues.queues:
+            circuit.queues.set(circuit.queue)
+
+        try:
+            cpu_backend = NumbaBackend()
+            cpu_backend.set_precision(self.precision)
+            ops = MultiGpuOps(self, cpu_backend, circuit)
+            
+            if initial_state is None:
+                # Generate pieces for |000...0> state
+                pieces = [cpu_backend.zero_state(circuit.nlocal)]
+                pieces.extend(np.zeros(2 ** circuit.nlocal, dtype=self.dtype) for _ in range(circuit.ndevices - 1))
+            elif isinstance(initial_state, CircuitResult):
+                # TODO: Implement this
+                raise_error(NotImplementedError)
+            elif isinstance(initial_state, self.tensor_types):
+                pieces = ops.to_pieces(initial_state)
+            else:
+                raise_error(TypeError, "Initial state type {} is not supported by "
+                                       "distributed circuits.".format(type(initial_state)))
+
+            special_gates = iter(circuit.queues.special_queue)
+            for i, queues in enumerate(circuit.queues.queues):
+                if queues: # standard gate
+                    pieces = ops.joblib_execute(pieces, queues)
+                    #config = circuit.queues.device_to_ids.items()
+                    #for device, ids in config:
+                    #    pieces = ops.apply_gate(pieces, queues, ids, device)
+                    #pool = joblib.Parallel(n_jobs=len(circuit.accelerators), prefer="threads")
+                    #pool(joblib.delayed(ops.apply_gate)(pieces, queues, ids, device) for device, ids in config)
+
+                else: # special gate
+                    gate = next(special_gates)
+                    if isinstance(gate, tuple): # SWAP global-local qubit
+                        global_qubit, local_qubit = gate
+                        pieces = ops.swap(pieces, global_qubit, local_qubit)
+                    else:
+                        pieces = ops.apply_special_gate(pieces, gate)
+            
+            for gate in special_gates: # pragma: no cover
+                pieces = ops.apply_special_gate(pieces, gate)
+
+            circuit._final_state = CircuitResult(self, circuit, pieces, nshots)
+            return circuit._final_state
+        
+        except self.oom_error:
+            raise_error(RuntimeError, "State does not fit in memory during distributed "
+                                      "execution. Please create a new circuit with "
+                                      "different device configuration and try again.")
+
+    def get_state_tensor(self, result):
+        if isinstance(result.execution_result, list):
+            # transform distributed state pieces to tensor
+            ops = MultiGpuOps(self, NumbaBackend(), result.circuit)
+            return ops.to_tensor(result.execution_result)
+        else:
+            return super().get_state_tensor(result)
+
     #def calculate_symbolic(self, state, nqubits, decimals=5, cutoff=1e-10, max_terms=20): Inherited from ``NumpyBackend``
 
     #def calculate_symbolic_density_matrix(self, state, nqubits, decimals=5, cutoff=1e-10, max_terms=20): Inherited from ``NumpyBackend``
@@ -561,3 +627,117 @@ class CupyBackend(NumpyBackend):
 
         raise_error(NotImplementedError, "Hamiltonian matmul to {} not "
                                          "implemented.".format(type(o)))
+
+
+class MultiGpuOps:
+
+    def __init__(self, backend, cpu_backend, circuit):
+        self.backend = backend
+        self.circuit = circuit
+        self.cpu_ops = cpu_backend.ops
+    
+    def transpose_state(self, pieces, state, nqubits, order):
+        original_shape = state.shape
+        state = state.ravel()
+        # always fall back to numba CPU backend because for ops not implemented on GPU
+        state = self.cpu_ops.transpose_state(tuple(pieces), state, nqubits, order)
+        return np.reshape(state, original_shape)
+
+    def to_pieces(self, state):
+        nqubits = self.circuit.nqubits
+        qubits = self.circuit.queues.qubits
+        shape = (self.circuit.ndevices, 2 ** self.circuit.nlocal)
+        state = np.reshape(self.backend.to_numpy(state), shape)
+        pieces = [state[i] for i in range(self.circuit.ndevices)]
+        new_tensor = np.zeros(shape, dtype=state.dtype)
+        new_tensor = self.transpose_state(pieces, new_tensor, nqubits, qubits.transpose_order)
+        for i in range(self.circuit.ndevices):
+            pieces[i] = new_tensor[i]
+        return pieces
+
+    def to_tensor(self, pieces):
+        nqubits = self.circuit.nqubits
+        qubits = self.circuit.queues.qubits
+        if qubits.list == list(range(self.circuit.nglobal)):
+            tensor = np.concatenate([x[np.newaxis] for x in pieces], axis=0)
+            tensor = np.reshape(tensor, (2 ** nqubits,))
+        elif qubits.list == list(range(self.circuit.nlocal, self.circuit.nqubits)):
+            tensor = np.concatenate([x[:, np.newaxis] for x in pieces], axis=1)
+            tensor = np.reshape(tensor, (2 ** nqubits,))
+        else: # fall back to the transpose op
+            tensor = np.zeros(2 ** nqubits, dtype=self.backend.dtype)
+            tensor = self.transpose_state(pieces, tensor, nqubits, qubits.reverse_transpose_order)
+        return tensor
+
+    def apply_gate_failing(self, pieces, queues, ids, device):
+        """Method that is parallelized using ``joblib``."""
+        for i in ids:
+            device_id = int(device.split(":")[-1]) % self.backend.ngpus
+            with self.backend.cp.cuda.Device(device_id):
+                piece = self.backend.cast(pieces[i])
+                for gate in queues[i]:
+                    piece = self.backend.apply_gate(gate, piece, self.circuit.nlocal)
+            pieces[i] = self.backend.to_numpy(piece)
+            del(piece)
+        return pieces
+
+    def apply_gates(self, state, gates, device):
+        device_id = int(device.split(":")[-1]) % self.backend.ngpus
+        with self.backend.cp.cuda.Device(device_id):
+            state = self.backend.cast(state)
+            for gate in gates:
+                state = self.backend.apply_gate(gate, state, self.circuit.nlocal)
+        return state
+
+    def joblib_execute(self, pieces, queues):
+        def device_job(ids, device):
+            for i in ids:
+                piece = self.apply_gates(pieces[i], queues[i], device)
+                pieces[i] = self.backend.to_numpy(piece)
+                del(piece)
+
+        for device, ids in self.circuit.queues.device_to_ids.items():
+            device_job(ids, device)
+       
+        #pool = joblib.Parallel(n_jobs=len(self.calc_devices),
+        #                       prefer="threads")
+        #pool(joblib.delayed(device_job)(ids, device)
+        #     for device, ids in self.queues.device_to_ids.items())
+        return pieces
+
+    def apply_special_gate(self, pieces, gate):
+        """Executes special gates on CPU.
+
+        Currently special gates are ``Flatten`` or ``CallbackGate``.
+        This method calculates the full state vector because special gates
+        are not implemented for state pieces.
+        """
+        from qibo.gates import CallbackGate
+        # Reverse all global SWAPs that happened so far
+        pieces = self.revert_swaps(pieces, reversed(gate.swap_reset))
+        state = self.to_tensor(pieces)
+        if isinstance(gate, CallbackGate):
+            gate.apply(self.backend, state, self.circuit.nqubits)
+        else:
+            state = gate.apply(self.backend, state, self.circuit.nqubits)
+            pieces = self.to_pieces(state)
+        # Redo all global SWAPs that happened so far
+        pieces = self.revert_swaps(pieces, gate.swap_reset)
+        return pieces
+
+    def swap(self, pieces, global_qubit, local_qubit):
+        m = self.circuit.queues.qubits.reduced_global.get(global_qubit)
+        m = self.circuit.nglobal - m - 1
+        t = 1 << m
+        for g in range(self.circuit.ndevices // 2):
+            i = ((g >> m) << (m + 1)) + (g & (t - 1))
+            local_eff = self.circuit.queues.qubits.reduced_local.get(local_qubit)
+            self.cpu_ops.swap_pieces(pieces[i], pieces[i + t], local_eff, self.circuit.nlocal)
+        return pieces
+
+    def revert_swaps(self, pieces, swap_pairs):
+        for q1, q2 in swap_pairs:
+            if q1 not in self.circuit.queues.qubits.set:
+                q1, q2 = q2, q1
+            pieces = self.swap(pieces, q1, q2)
+        return pieces
