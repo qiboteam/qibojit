766ddcc4603a2703893802b8ede8c0863e430cc3
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2025-05-07 12:25:32 +0200

    fix: bugfix + perf improvement of numba packbits

--- src/qibojit/backends/clifford_operations_cpu.py
+++ src/qibojit/backends/clifford_operations_cpu.py
@@ -265,12 +265,11 @@ def _packbits(array, axis):
     array = np.reshape(array, (dim, n))
     packed_len = (n + 7) // 8
     out = np.zeros((dim, packed_len), dtype=np.uint8)
-    tmp = np.arange(n, dtype=np.uint8)
-    byte_idx = tmp // 8
-    bit_idx = 7 - (tmp % 8)
     for j in prange(dim):  # pylint: disable=not-an-iterable
         for i in prange(n):  # pylint: disable=not-an-iterable
-            out[j, byte_idx[i]] |= array[j, i] << bit_idx[i]
+            byte_idx = i // 8
+            bit_idx = 7 - (i % 8)
+            out[j, byte_idx] |= array[j, i] << bit_idx
     out = np.reshape(out, shape[:-1] + (packed_len,))
     return np.swapaxes(out, axis, -1)
 
